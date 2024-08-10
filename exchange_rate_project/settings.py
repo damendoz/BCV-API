b9394e06f1befa72f98f5ebba0e3370a12462a9d
Daniel Mendoza Arreaza <87989239+damendoz@users.noreply.github.com>
2024-08-10 18:44:17 -0400

    databases

--- exchange_rate_project/settings.py
+++ exchange_rate_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +78,7 @@ WSGI_APPLICATION = 'exchange_rate_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
