6a62d9130a52cce32a7d0dbff67a54bab2aac663
Daniel Mendoza Arreaza <87989239+damendoz@users.noreply.github.com>
2024-08-10 19:30:33 -0400

    routting

--- exchange_rate/urls.py
+++ exchange_rate/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ExchangeRateView
 
 urlpatterns = [
-    path('exchange-rate/', ExchangeRateView.as_view(), name='exchange-rate'),
+    path('', ExchangeRateView.as_view(), name='exchange-rate'),
 ]
--- exchange_rate_project/urls.py
+++ exchange_rate_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('exchange_rate.urls')),
+    path('', include('exchange_rate.urls')),
 ]
