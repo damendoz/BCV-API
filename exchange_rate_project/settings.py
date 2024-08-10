7e6f695d3f8cb6372a7c8baace7660e39ae6db42
Daniel Mendoza Arreaza <87989239+damendoz@users.noreply.github.com>
2024-08-10 19:43:32 -0400

    rendering json

--- exchange_rate_project/settings.py
+++ exchange_rate_project/settings.py
@@ -81,6 +81,15 @@ DATABASES = {
     'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
